8c5cda52278bb5c2bdefd1fd81820a20aaf0f82f
Matthew Tan <tanmatt804@gmail.com>
2021-10-06 14:30:45 +0000

    finished

--- index.js
+++ index.js
@@ -1,8 +1,31 @@
 //1
-function absoluteValue(){
-  
+function absoluteValue(num){
+  if (typeof num == 'number') {
+        if(num < 0){
+            return -1*num;}
+        
+        else if(num >= 0) {
+            return num; }
+                    }
+  else {
+      return null;
+        }
 }
 
+
+
+
+//2
+function multiplesOfFourAndSix(arr){
+    let hold = [];
+    for(let i = 1; i <= 100; i++){
+        if(i % 4  === 0 && i % 6 === 0){
+            hold.push(i);
+        }
+    }return hold;
+}
+console.log(absoluteValue(-99))
+console.log(multiplesOfFourAndSix())
 
 
 
